c2807f015dc5ef4a4f079fafed114cf9f3a367ae
Chin Tian <kerchintian@gmail.com>
2019-04-09 14:43:43 +0800

    OCV setup

--- app/src/main/java/com/example/mlkitocv/MainActivity.java
+++ app/src/main/java/com/example/mlkitocv/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.mlkitocv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug())
+            Log.i(TAG, "OpenCV loaded");
     }
 }
