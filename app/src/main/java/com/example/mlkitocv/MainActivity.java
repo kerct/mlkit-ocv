96910e3f0c423464b286cca30c026ac54287a831
Chin Tian <kerchintian@gmail.com>
2019-04-10 15:13:20 +0800

    add permissions to manifest + recognise activity

--- app/src/main/java/com/example/mlkitocv/MainActivity.java
+++ app/src/main/java/com/example/mlkitocv/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mlkitocv;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import org.opencv.android.OpenCVLoader;
 
@@ -16,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
 
         if(OpenCVLoader.initDebug())
             Log.i(TAG, "OpenCV loaded");
+
+        Button recognizeButton = findViewById(R.id.recognizeButton);
+        Button trainingButton = findViewById(R.id.trainingButton);
+
+        recognizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Recognise.class));
+            }
+        });
+
+        trainingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(MainActivity.this, NameActivity.class));
+            }
+        });
     }
 }
