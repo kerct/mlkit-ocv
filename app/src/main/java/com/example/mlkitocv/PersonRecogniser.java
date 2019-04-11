4b6162aa3b1aa13248960cb6a338804d65f63f5a
Chin Tian <kerchintian@gmail.com>
2019-04-11 14:34:54 +0800

    lbph

--- app/src/main/java/com/example/mlkitocv/PersonRecogniser.java
+++ app/src/main/java/com/example/mlkitocv/PersonRecogniser.java
@@ -1,12 +1,16 @@
 package com.example.mlkitocv;
 
-import org.opencv.face.FaceRecognizer;
+import org.bytedeco.opencv.opencv_face.FaceRecognizer;
+import org.bytedeco.opencv.opencv_face.LBPHFaceRecognizer;
 
 public class PersonRecogniser {
     private FaceRecognizer fr;
+    private String path;
+    private int count = 0;
 
     PersonRecogniser(String path) {
-
+        fr = new LBPHFaceRecognizer();
+        this.path = path;
     }
 
 }
