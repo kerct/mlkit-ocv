c2f84a605d04d7fa09bf58a644738ae5b5e3e8d1
Chin Tian <kerchintian@gmail.com>
2019-04-11 14:08:29 +0800

    javacv stuff

---
+++ app/src/main/java/com/example/mlkitocv/PersonRecogniser.java
@@ -0,0 +1,12 @@
+package com.example.mlkitocv;
+
+import org.opencv.face.FaceRecognizer;
+
+public class PersonRecogniser {
+    private FaceRecognizer fr;
+
+    PersonRecogniser(String path) {
+
+    }
+
+}
