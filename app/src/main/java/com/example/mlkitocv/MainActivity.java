d61c91490194d39b106faa4b1bc47d3ea7c644ba
Chin Tian <kerchintian@gmail.com>
2019-04-22 12:28:38 +0800

    name activity UI

--- app/src/main/java/com/example/mlkitocv/MainActivity.java
+++ app/src/main/java/com/example/mlkitocv/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         trainingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //startActivity(new Intent(MainActivity.this, NameActivity.class));
+                startActivity(new Intent(MainActivity.this, NameActivity.class));
             }
         });
     }
